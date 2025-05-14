03d75f4fa67e5823decb572ff8fdb0f3e2427603
amiralidarghadami <158480342+amiralidarghadami@users.noreply.github.com>
2025-05-14 17:36:23 +0330

    add fonts
    add fonts

--- resources/views/layouts/admin/links.blade.php
+++ resources/views/layouts/admin/links.blade.php
@@ -2,6 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
 <title>Sales Admin | CORK </title>
+<link rel="stylesheet"  href="{{asset('fonts/fontiran.css')}}"/>
+<link rel="stylesheet"  href="{{asset('fonts/stylef.css')}}"/>
 <link rel="icon" type="image/x-icon" href="{{asset('admin/src/assets/img/favicon.ico')}}"/>
 <link href="{{asset('admin/layouts/vertical-dark-menu/css/light/loader.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('admin/layouts/vertical-dark-menu/css/dark/loader.css')}}" rel="stylesheet" type="text/css" />
