26f32512270afcd05cefff4cda6ec7161285e731
amiralidarghadami <158480342+amiralidarghadami@users.noreply.github.com>
2025-05-24 20:28:55 +0330

    end part of country in admin panel

--- resources/views/livewire/admin/country/index.blade.php
+++ resources/views/livewire/admin/country/index.blade.php
@@ -19,13 +19,13 @@
                         </div>
                     </div>
                     @error('name')
-                    <div class="alert alert-light-danger alert-dismissible fade show border-0 mb-4" role="alert">
+                    <div class="alert alert-light-danger alert-dismissible fade show border-0 mb-4" role="alert" wire:loading.remove>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
                         <strong> اخطار! </strong>{{$message}}</button>
                     </div>
                     @enderror
-                    <button type="submit" class="btn btn-primary _effect--ripple waves-effect waves-light">ثبت
-                    </button>
+                    <button type="submit" class="btn btn-info btn-lg mb-2 me-4"><svg wire:loading xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-loader spin me-2"><line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg> ثبت</button>
+
                 </form>
             </div>
         </div>
@@ -56,7 +56,7 @@
                         @foreach($countries as $country)
                             <tr>
                                 <td>
-                                    {{$loop->index +1}}
+                                    {{$loop->iteration + $countries->firstItem() -1}}
                                 </td>
                                 <td>
                                     <div class="media">
@@ -99,8 +99,8 @@
                         @endforeach
                         </tbody>
                     </table>
+                    {{$countries->links('layouts.admin.pagination')}}
                 </div>
-                {{$countries->links('layouts.admin.pagination')}}
             </div>
         </div>
     </div>
